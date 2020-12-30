6cb14bc473f6908d5d368e3121c37a1c0ad8bf41
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2020-12-31 01:38:24 +0500

    Player Targetting Enemy Game

---
+++ A2 Q3/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace A2_Q3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(374, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score : 0";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::A2_Q3.Properties.Resources._123;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ A2 Q3/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A2_Q3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        PictureBox[] target = new PictureBox[4];
+        PictureBox hunter = new PictureBox();
+        int score;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < target.Length; i++)
+            {
+                target[i] = new PictureBox();
+                target[i].Location = new Point(r.Next(1, 500), 150);
+                target[i].Size = new Size(75,85);
+                target[i].ImageLocation = "D:/ahmas2.PNG";
+                target[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(target[i]);
+                target[i].BringToFront();
+            }
+            hunter.Location = new Point(800, 150);
+            hunter.Size = new Size(120, 120);
+            hunter.ImageLocation = "D:/ahmas.PNG";
+            hunter.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(hunter);
+            hunter.BringToFront();
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            int max = this.Width;
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (target[i].Left > max)
+                {
+
+                    target[i].Location = new Point(0, 150);
+                    hunter.Location = new Point(700, 100);
+                }
+                else
+                {
+                    int x = r.Next(0, 20);
+                    target[i].Location = new Point(target[i].Location.X + x, target[i].Location.Y);
+                    hunter.Location = new Point(hunter.Location.X - x, hunter.Location.Y);
+                    if (target[i].Bounds.IntersectsWith(hunter.Bounds) && target[i].Visible)
+                    {
+                        target[i].ImageLocation = "D:/dead.PNG";
+                        target[i].Dispose();
+                        score = score + 1;
+                        label1.Text = "Score: " + score;
+                        target[i].BringToFront();
+
+
+                    }
+                }
+
+            }
+            if (target == null)
+            {
+                timer1.Stop();
+            }
+
+
+
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
